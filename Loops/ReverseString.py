879f567c01ad054cb19cfd1bcd4f20be7ed9290c
adhiraj-code <alkarathore87@gmail.com>
2026-01-31 11:33:13 +0530

    Add files via upload

---
+++ Loops/ReverseString.py
@@ -0,0 +1,8 @@
+name = input("Enter you name : ")
+
+reverse = ""
+
+for i in name:
+    reverse = i + reverse
+
+print(reverse)
---
+++ Loops/SumOfWholeNumbers.py
@@ -0,0 +1,6 @@
+number = int(input("Enter the number : "))
+sum = 0
+
+for i in range(1, number + 1):
+    sum = sum + i
+print("The sum is ", sum)
